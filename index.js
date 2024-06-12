73d052ab328700c7d6dc96db1601ce27f755603a
warfdev <129093003+warfdev@users.noreply.github.com>
2024-06-12 07:22:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,24 +25,16 @@ class Dini {
 
     set(name, value) {
         try {
-            // Read existing data from the file
             let data = fs.readFileSync(this.filePath, "utf8");
-
-            // Parse the data as INI format
             let parsedData = this.parseINI(data);
 
-            // Check if the section exists, if not create it
             if (!parsedData[""]) {
                 parsedData[""] = {};
             }
 
-            // Set the value for the specified name
             parsedData[""][name] = value;
-
-            // Convert the data back to INI format
             let newData = this.stringifyINI(parsedData);
 
-            // Write the updated data to the file
             fs.writeFileSync(this.filePath, newData, "utf8");
         } catch (err) {
             console.error("Error while setting value in INI file:", err);
@@ -114,7 +106,7 @@ class Dini {
             return null;
         }
     }
-    
+
     clear() {
         try {
             fs.writeFileSync(this.filePath, "", "utf8");
@@ -128,7 +120,7 @@ class Dini {
         try {
             let data = fs.readFileSync(this.filePath, "utf8");
             let parsedData = this.parseINI(data);
-    
+
             if (parsedData[""] && parsedData[""][oldName]) {
 
                 let value = parsedData[""][oldName];
@@ -136,10 +128,10 @@ class Dini {
 
                 parsedData[""][newName] = value;
                 let newData = this.stringifyINI(parsedData);
-    
+
                 fs.writeFileSync(this.filePath, newData, "utf8");
-    
-                console.log(`Variable '${oldName}' renamed to '${newName}' successfully.`);
+
+                //DEBUG console.log(`Variable '${oldName}' renamed to '${newName}' successfully.`);
             } else {
                 console.error(`Variable '${oldName}' not found.`);
             }
@@ -147,6 +139,164 @@ class Dini {
             console.error("Error while renaming variable in INI file:", err);
         }
     }
+    
+    raw(command) {
+      try {
+          const regexSet = /SELECT_KEY\s+"([^"]+)"\s+OPERATOR\s+(set)\s+VALUE\s+"([^"]+)"/;
+          const regexGetHasDelete = /SELECT_KEY\s+"([^"]+)"\s+OPERATOR\s+(get|has|delete)/;
+          const regexClear = /OPT\s+"clear"/;
+  
+          if (regexSet.test(command)) {
+              const match = command.match(regexSet);
+  
+              if (!match || match.length !== 4) {
+                  console.error("Invalid set command format.");
+                  return;
+              }
+  
+              const name = match[1];
+              const operator = match[2];
+              const value = match[3];
+  
+              this.set(name, value);
+          } else if (regexGetHasDelete.test(command)) {
+              const match = command.match(regexGetHasDelete);
+  
+              if (!match || match.length !== 3) {
+                  console.error("Invalid get/has/delete command format.");
+                  return;
+              }
+  
+              const name = match[1];
+              const operator = match[2];
+  
+              switch (operator) {
+                  case "get":
+                      return this.get(name);
+                  case "has":
+                      return this.has(name);
+                  case "delete":
+                      this.delete(name);
+                      break;
+                  default:
+                      console.error("Invalid operator:", operator);
+              }
+          } else if (regexClear.test(command)) {
+              this.clear();
+          } else {
+              console.error("Invalid command format.");
+              return;
+          }
+      } catch (err) {
+          console.error("Error while executing raw command:", err);
+      }
+    }
+    
+    
+    createObject(name, object) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          parsedData[name] = object;
+          let newData = this.stringifyINI(parsedData);
+          fs.writeFileSync(this.filePath, newData, "utf8");
+          //DEBUG console.log(`Object '${name}' created successfully.`);
+      } catch (err) {
+          console.error(`Error while creating object '${name}':`, err);
+      }
+    }
+
+    getObject(name) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          return parsedData[name] || null;
+      } catch (err) {
+          console.error(`Error while getting object '${name}':`, err);
+          return null;
+      }
+    }
+
+    hasObject(name) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          return !!parsedData[name];
+      } catch (err) {
+          console.error(`Error while checking if object '${name}' exists:`, err);
+          return false;
+      }
+    }
+
+    addObject(name, key, value) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          if (!parsedData[name]) {
+              parsedData[name] = {};
+          }
+          parsedData[name][key] = value;
+          let newData = this.stringifyINI(parsedData);
+          fs.writeFileSync(this.filePath, newData, "utf8");
+          //DEBUG console.log(`Key '${key}' with value '${value}' added to object '${name}' successfully.`);
+      } catch (err) {
+          console.error(`Error while adding key-value pair to object '${name}':`, err);
+      }
+    }
+
+    deleteObject(name) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          delete parsedData[name];
+          let newData = this.stringifyINI(parsedData);
+          fs.writeFileSync(this.filePath, newData, "utf8");
+          //DEBUG console.log(`Object '${name}' deleted successfully.`);
+      } catch (err) {
+          console.error(`Error while deleting object '${name}':`, err);
+      }
+    }
+
+    removeObject(name, object) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          
+          if(parsedData[name]) {
+              delete parsedData[name][object];
+              let newData = this.stringifyINI(parsedData);
+              fs.writeFileSync(this.filePath, newData, "utf8");
+              //DEBUG console.log(`Object '${object}' removed from '${name}' successfully.`);
+          } else {
+              console.error(`Object '${name}' not found.`);
+          }
+      } catch (err) {
+          console.error(`Error while removing object '${object}' from '${name}':`, err);
+      }
+    }
+
+    setObject(name, object, newValue) {
+      try {
+          let data = fs.readFileSync(this.filePath, "utf8");
+          let parsedData = this.parseINI(data);
+          
+          if(parsedData[name]) {
+              parsedData[name][object] = newValue;
+          } else {
+              parsedData[name] = { [object]: newValue };
+          }
+          
+          let newData = this.stringifyINI(parsedData);
+          fs.writeFileSync(this.filePath, newData, "utf8");
+          //DEBUG console.log(`Object '${object}' set to '${newValue}' in '${name}' successfully.`);
+      } catch (err) {
+          console.error(`Error while setting object '${object}' to '${newValue}' in '${name}':`, err);
+      }
+    }
+    
+  
+  
+  // end
 }
 
 module.exports = Dini;
